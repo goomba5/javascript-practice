75807c3ee649f0c4932a700e14256e0a0df4ebc8
Gary Henderson <hendergy@gmail.com>
2019-10-08 23:36:04 -0400

    created checkArray function

--- array-five/array-five.js
+++ array-five/array-five.js
@@ -4,5 +4,27 @@
 let pool = [1, 2, 3, 4];
 
 checkArray = (value) => {
-
+    let result;
+    for (let i = 0; i < pool.length; i++) {
+        if (value === pool[i]) {
+            result = "The number " + value + " is in the array! Hooray!";
+            break;
+        }
+        else {
+            result = "The number " + value + " is not in the array. Bummer.";
+        }
+    }
+    console.log(result);
 }
+
+// test 1
+checkArray(5);
+
+// test 2
+checkArray(3);
+
+// test 3
+checkArray(7);
+
+// test 4
+checkArray(4);
