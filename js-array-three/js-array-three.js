96d47618c6cb19062bafdde7bdd77376302cfce3
Gary Henderson <hendergy@gmail.com>
2019-10-10 18:17:35 -0400

    added loop to print first n elements of array

--- js-array-three/js-array-three.js
+++ js-array-three/js-array-three.js
@@ -1,5 +1,5 @@
 /*  create a function that returns the first element of an array, and also accepts a
-    parameter which returns the first 'x' elements in the
+    parameter which returns the first 'x' elements in the array
 */
 
 // return the first element in an array
@@ -8,20 +8,30 @@ const firstArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const secondArr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 const thirdArr = [100, 200, 300, 400, 500, 600, 700, 800, 900, 10000];
 
-// regular function
-function getFirstElement(arr, span) {
+// ES6 arrow function
+getFirstElement = (arr, elements) => {
+
+    // prints the first element of the array
+    console.log("The first element is: ");
     let firstElement = arr[0];
 
     console.log(firstElement);
+
+    // prints the first 'n' elements of the array
+    console.log("The first " + elements + " elements are: ");
+
+    for (let i = 0; i < elements; i++) {
+        console.log(arr[i]);
+    }
 }
 
 /* TESTS */
 
 // test 1
-getFirstElement(firstArr);
+getFirstElement(firstArr, 3);
 
 // test 2
-getFirstElement(secondArr);
+getFirstElement(secondArr, 5);
 
 // test 3
-getFirstElement(thirdArr);
+getFirstElement(thirdArr, 4);
