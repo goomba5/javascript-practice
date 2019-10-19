1f26eaaf8b81e902fc747921d74124bcd6505532
Gary Henderson <hendergy@gmail.com>
2019-10-18 20:42:35 -0400

    final solution

--- js-fndml-one/js-fndml-one.js
+++ js-fndml-one/js-fndml-one.js
@@ -1,14 +1,28 @@
 // write a javascript function which compares two objects
 // and determines if each object has the same values
 
-const objectOne = { 1: "Gary", 2: "Kelly" };
+const objectOne = { 1: "Gary", 2: "Kelly", 3: "Mom" };
 const objectTwo = { 1: "Gary", 2: "Kelly" };
+const objectThree = { 1: "Blah", 2: "Blah", 3: "Blah", 4: "Blah" };
+const objectFour = { 1: "Blah", 2: "Blah", 3: "Blah", 4: "Blah" };
 
-Object.equals = (obj1, obj2) => {
-    if (obj1 === obj2) {
-        return true;
-    }
-    return false;
+areObjectsEqual = (obj1, obj2) => {
+
+    let isEqual = Object.keys(obj1).every(key => obj2.hasOwnProperty(key) && obj2[key] === obj1[key]);
+
+    // Object.keys() creates an array containing each of the keys from obj1
+    // Array.every() is used to test each key in the obj1 array
+    // If the second object's KEYS equal the first object's KEYS
+    // AND the second object's VALUES equal the first object's VALUES
+    // then isEqual will return true, else false
+    console.log(isEqual);
 }
 
-Object.equals(objectOne, objectTwo);
+// test 1
+areObjectsEqual(objectOne, objectTwo); // false
+
+// test 2
+areObjectsEqual(objectOne, objectFour); // false
+
+// test 3
+areObjectsEqual(objectThree, objectFour); // true
