247f783cfd3b7808a0ba96271ebf5ae49fa624fc
Gary Henderson <hendergy@gmail.com>
2019-10-29 22:41:34 -0400

    created array of objects

---
+++ js-object-ten/js-object-ten.js
@@ -0,0 +1,19 @@
+// sort the array of objects
+
+let library = [
+    {
+        author: "Jon Duckett",
+        title: "HTML & CSS: Design and Build Websites",
+        readingStatus: true
+    },
+    {
+        author: "Jon Duckett",
+        title: "JavaScript & JQuery",
+        readingStatus: false
+    },
+    {
+        author: "Ta-Nehisi Coates",
+        title: "Black Panther: A Nation Under Our Feet, Book One",
+        readingStatus: true
+    }
+]
