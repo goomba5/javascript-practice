22daea5ebb880da8e92b568ef7213e64e3268e3f
Gary Henderson <hendergy@gmail.com>
2019-11-05 23:12:02 -0500

    add flashcard styles, displays random numbers with operator

--- flashcard-game/flashcard-game.js
+++ flashcard-game/flashcard-game.js
@@ -1,18 +1,60 @@
 let num;
+let firstNumber = document.getElementById("firstNumber");
+let secondNumber = document.getElementById("secondNumber");
+let operator = document.getElementById("operator");
+let input = document.getElementById("userInput").value;
+let selection = document.getElementById("selection");
+let selectOption = selection.value;
+let operatorDisplay;
 
 randomNumber = () => {
     num = Math.floor((Math.random() * 20) + 1);
     console.log(num);
+    return num;
 }
 
-firstNumber = () => {
-    let numberOne = randomNumber().toString();
-    document.getElementById("firstNumber").innerHTML = numberOne;
+firstOperand = () => {
+    let num1 = randomNumber();
+    return num1;
 }
 
-getInput = () => {
-    let input = document.getElementById("userInput").value;
+secondOperand = () => {
+    let num2 = randomNumber();
+    return num2;
+}
+
+getOperator = (selectOption) => {
+    switch (selectOption) {
+        case "addition":
+            operatorDisplay = "+";
+            break;
+        case "subtraction":
+            operatorDisplay = "-";
+            break;
+        case "multiplication":
+            operatorDisplay = "X"
+            break;
+        case "division":
+            operatorDisplay = "/";
+            break;
+        // case "Greater Than":
+        //     operatorDisplay = ">";
+        //     break;
+        // case "Lesser Than":
+        //     operatorDisplay = "<";
+        //     break;
+        default:
+            operatorDisplay = "+";
+            break;
+    }
+    return operatorDisplay;
+}
+
+calculateAnswer = () => {
     let answer = parseInt(input);
-    let sum = answer + 5;
-    alert(sum);
+
 }
+
+firstNumber.innerHTML = firstOperand();
+secondNumber.innerHTML = secondOperand();
+operator.innerHTML = getOperator();
