9bd2efe65b6fb5d8f66fd6eab0f25a47dd61bb91
Gary Henderson <hendergy@gmail.com>
2019-11-08 08:39:41 -0500

    updated console log messages to display original num and reverse num

--- js-func-one/js-func-one.js
+++ js-func-one/js-func-one.js
@@ -1,5 +1,9 @@
 // write a JavaScript function that reverses a number
 
+originalNum = (num) => {
+    console.log("Original number: " + num);
+}
+
 reverseNumber = (num) => {
     let numArray = [];
     numToStr = num.toString();
@@ -9,17 +13,20 @@ reverseNumber = (num) => {
     }
     let str = numArray.reverse().join("");
     let newNum = parseInt(str);
-    console.log(newNum);
+    console.log("Reversed number: " + newNum);
 }
 
 // test 1
 console.log("Test 1");
+originalNum(257);
 reverseNumber(257);
 
 // test 2
 console.log("Test 2");
+originalNum(68732);
 reverseNumber(68732);
 
 // test 3
 console.log("Test 3");
+originalNum(100000000002);
 reverseNumber(100000000002);
