29b55ad31b5aa03905c28fed7530094adf997bef
Gary Henderson <hendergy@gmail.com>
2019-12-26 23:52:35 -0500

    created dog api call using hooks

--- dog-api/src/pages/index.js
+++ dog-api/src/pages/index.js
@@ -1,21 +1,26 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import DogList from "../components/list"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <DogList />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const [useDogList, setDogList] = useState(false);
+  const [hasError, setError] = useState({});
+
+  async function getData() {
+    const response = await fetch("https://dog.ceo/api/breeds/list/all");
+    response
+      .json()
+      .then(res => setDogList(res))
+      .catch(err => setError(err))
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+}
 
 export default IndexPage
